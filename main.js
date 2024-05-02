90762656528b3b62a3a500879cb44d9df70dcd27
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-05-02 19:47:40 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const carroDoJoao = {
+    modelo: 'fiesta',
+    fabricante: 'ford',
+    anoModelo: 2024,
+    anoFabricacao: 2023,
+    acelerar: function() {
+        console.log("vruum");
+    }
+}
