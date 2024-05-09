db07fe8f196a457f03d923c5f0f329fbd2897f3d
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-05-09 18:59:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,52 @@
-const carroDoJoao = {
-    modelo: 'fiesta',
-    fabricante: 'ford',
-    anoModelo: 2024,
-    anoFabricacao: 2023,
-    acelerar: function() {
-        console.log("vruum");
+class Veiculo {
+    constructor(volkswagen, gol) {
+      this.marca = volkswagen;
+      this.modelo = gol;
     }
-}
+  
+    acelerar() {
+      console.log(`${this.modelo} está acelerando.`);
+    }
+  
+    frear() {
+      console.log(`${this.modelo} está freando.`);
+    }
+  
+
+    ligar() {
+      throw new Error(' "ligar" ');
+    }
+  }
+  
+
+  class Carro extends Veiculo {
+    constructor(volkswagen, gol, vermelha) {
+      super(volkswagen, gol);
+      this.cor = vermelha;
+    }
+  
+    ligar() {
+      console.log(`${this.modelo} ligou o motor.`);
+    }
+  }
+  
+
+  class Moto extends Veiculo {
+    constructor(honda, start, 160) {
+      super(honda, start);
+      this.cilindradas = 160;
+    }
+  
+    ligar() {
+      console.log(`${this.modelo} ligou o motor.`);
+    }
+  }
+  
+
+  let carro1 = new Carro('Toyota', 'Corolla', 'Preto');
+  let moto1 = new Moto('Honda', 'CBR', 600);
+  let carro2 = new Carro('Ford', 'Fiesta', 'Prata');
+  
+  carro1.acelerar();
+  moto1.frear();
+  carro2.ligar();
